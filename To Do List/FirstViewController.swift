47ea457362e89e4b80da4a00ae493d527dda5a4f
Vanessa Chu <vanessachu@Vanessas-Air.hitronhub.home>
2017-11-05 19:55:40 -0800

    complete project files

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,52 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var item = [String]()
+    @IBOutlet var table: UITableView!
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return item.count
+    }
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = item[indexPath.row]
+        print(Int(indexPath.row))
+        print(item.count)
+        
+        return cell
+    }
+    internal func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    internal func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            item.remove(at: indexPath.item)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        print(item)
+        UserDefaults.standard.set(item, forKey:"item")
+        
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let itemObject = UserDefaults.standard.object(forKey: "item")
+        
+        if let tempItem = itemObject as? [String]{
+            item = tempItem
+            print(item)
+        }
+        table.reloadData()
+
     }
 
     override func didReceiveMemoryWarning() {
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var itemField: UITextField!
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+        let itemObject = UserDefaults.standard.object(forKey: "item")
+        
+        if itemField.text != ""{
+            var item: [String]
+            if let tempItem = itemObject as? [String]{
+                item = tempItem
+                item.append(itemField.text!)
+            }else{
+                item = [itemField.text!]
+            }
+            UserDefaults.standard.set(item, forKey:"item")
+            print(item)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
